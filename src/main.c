a958c8cb04f1551aee30d3cf40a7624a07e18c3f
Prad6sh <6pradosh@gmail.com>
2026-05-08 21:36:49 +0530

    test number loop

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    while(1)
+    {
+        printf("1\n");
+        Sleep(3000);
+
+        printf("2\n");
+        Sleep(3000);
+    }
+
+    return 0;
+}
